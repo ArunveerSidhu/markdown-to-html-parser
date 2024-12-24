b0a4ee8a3a56e4e2df4742405fd8498d7bea47fd
ArunveerSidhu <sidhu.arunveer@gmail.com>
2024-12-24 12:41:08 +0530

    made components

--- parser/src/App.jsx
+++ parser/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Convert from './components/Convert'
 
 function App() {
   return (
     <>
-      
+      <div className='h-screen w-screen bg-gray-800 flex items-center justify-center'>
+        <Convert />
+      </div>
     </>
   )
 }
---
+++ parser/src/components/Convert.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Convert() {
+  return (
+    <div>
+        <input 
+            type="text" 
+            name="" 
+            id="" 
+            placeholder='Type your markdown here...' 
+            className='size-96 font-mono text-center'
+        />
+    </div>
+  )
+}
+
+export default Convert
